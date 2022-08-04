82b2ac2d3448e9fe42035052c08b9e4b8ae4bca9
Biblbim <biblbim1994@gmail.com>
2022-08-04 21:39:43 +0500

    lesson_1.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_name = models.CharField(max_length=150, blank=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True)
