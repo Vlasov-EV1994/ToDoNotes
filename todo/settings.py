1ed76e1cd2c2e304bbc3e457854ddfe94c3e3d59
Biblbim <biblbim1994@gmail.com>
2022-09-08 21:19:10 +0500

    Lesson 6. Final

--- todo/settings.py
+++ todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     'users',
     'corsheaders',
@@ -54,7 +55,13 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 
 }
 
--- todoapp/tests.py
+++ todoapp/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+response = requests.post('http://127.0.0.1:8000/api-token-auth/', data={'username':
+'Biblbim', 'password': 'Q123$'})
+
+print(response.status_code)
+print(response.json()) #f803e302f19fba6da1b369d6c7203b7644f00bee
