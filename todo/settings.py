21ed4265dfa8d33be35d6938bb4e8b9ac815d5c9
Biblbim <biblbim1994@gmail.com>
2022-09-15 21:31:49 +0500

    Lesson 8.

--- todo/settings.py
+++ todo/settings.py
@@ -57,12 +57,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
 
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    #'DEFAULT_AUTHENTICATION_CLASSES': [
+     #   'rest_framework.authentication.BasicAuthentication',
+      #  'rest_framework.authentication.TokenAuthentication',
+       # 'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework_simplejwt.authentication.JWTAuthentication',
+    #]
 
 }
 
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,81 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import UserModelViewSet
+from .models import User
+from todoapp.views import ProjectCustomDjangoFilterViewSet
+from todoapp.models import Project
 
-# Create your tests here.
+class TestUserModelViewSet(TestCase):
+
+    def test_factory(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+
+        request = factory.post('/api/users/', {'username': 'UserOk'}, format='json')
+        view = UserModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_apiclient(self):
+        user = User.objects.create(username='user1', first_name='qwerty')
+        client = APIClient()
+        admin = User.objects.create_superuser('admin', 'admin@admin.com',
+                                              'admin123456')
+        client.login(username='admin', password='admin123456')
+        response = client.get(f'/api/users/{user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        client.logout()
+
+    def test_mixer(self):
+        user = mixer.blend(User)
+        admin = User.objects.create_superuser('admin', 'admin@admin.com',
+                                              'admin123456')
+        self.client.login(username='admin', password='admin123456')
+        response = self.client.get(f'/api/users/{user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user = User.objects.get(id=user.id)
+
+    def test_mixer_project(self):
+        project = mixer.blend(Project)
+        admin = User.objects.create_superuser('admin', 'admin@admin.com',
+                                              'admin123456')
+        self.client.login(username='admin', password='admin123456')
+        response = self.client.get(f'/api/project/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user = User.objects.get(id=project.id)
+
+    def test_create_admin(self):
+        factory = APIRequestFactory()
+
+        request = factory.post('/api/users/', {'username': 'pwfefw',
+                                               'first_name': 'wfwef',
+                                               'last_name': 'efwef',
+                                               'email': 'fwewefw@icloud.com'}, format='json')
+        admin = User.objects.create_superuser('admin', 'admin@admin.com',
+                                              'admin123456')
+        force_authenticate(request, admin)
+        view = UserModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class TestUser(APITestCase):
+    def test_APITC(self):
+        user = User.objects.create(username='Testuser', first_name='qwert', last_name='fffff', email='sdfqw@icloud.com')
+        admin = User.objects.create_superuser('admin', 'admin@admin.com',
+                                              'admin123456')
+        self.client.login(username='admin', password='admin123456')
+        response = self.client.get(f'/api/users/{user.id}/', {'username': 'xc'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user = User.objects.get(id=user.id)
+        self.assertEqual(user.username, 'Testuser')
